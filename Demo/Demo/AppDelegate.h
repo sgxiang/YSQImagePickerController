a190520f6eeb42d05ba37b910b6b8abbcdf2e86f
sgxiang <sgxiang1992@icloud.com>
2014-07-01 17:38:28 +0800

    init project

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by ysq on 14/7/1.
+//  Copyright (c) 2014年 ysq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by ysq on 14/7/1.
+//  Copyright (c) 2014年 ysq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
